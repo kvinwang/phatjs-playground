21dc58dded8afa8c6c37c92a81f19c9bb00a5c0f
Kevin Wang <wy721@qq.com>
2023-06-11 10:59:09 +0000

    Refine witness script

--- examples/github-actions-witness.js
+++ examples/github-actions-witness.js
@@ -3,41 +3,7 @@ function hex(bytes) {
         return ('0' + (byte & 0xFF).toString(16)).slice(-2);
     }).join('');
 }
-
-class ActionRun {
-    constructor(url) {
-        const { owner, repo, runId } = parseActionUrl(url);
-        this.owner = owner;
-        this.repo = repo;
-        this.runId = runId;
-        this.repoUrl = `https://api.github.com/repos/${owner}/${repo}`;
-        this.url = `https://api.github.com/repos/${owner}/${repo}/actions/runs/${runId}`;
-    }
-
-    get baseFileUrl() {
-        return `${this.repoUrl}/contents/`;
-    }
-    get baseFileUrl2() {
-        return `https://raw.githubusercontent.com/${this.owner}/${this.repo}/`;
-    }
-    get jobsUrl() {
-        return `${this.url}/jobs`;
-    }
-}
-
-function parseActionUrl(url) {
-    const match = url.match(/https:\/\/github.com\/([^\/]+)\/([^\/]+)\/actions\/runs\/(\d+)/);
-    if (!match) {
-        throw new Error("Invalid input url");
-    }
-    return {
-        owner: match[1],
-        repo: match[2],
-        runId: match[3],
-    }
-}
-
-function githubApiGet(url, token, raw) {
+function ghGet(url, token, raw) {
     const headers = {
         'Accept': 'application/vnd.github+json',
         'X-GitHub-Api-Version': '2022-11-28',
@@ -56,7 +22,7 @@ function githubApiGet(url, token, raw) {
         returnTextBody: true,
     });
     if (response.statusCode !== 200) {
-        throw new Error(`Bad status code: ${response.statusCode}`);
+        throw new Error(`Bad status code: ${response.statusCode}, url: ${url}`);
     }
     if (raw) {
         return response.body;
@@ -64,22 +30,35 @@ function githubApiGet(url, token, raw) {
     return JSON.parse(response.body);
 }
 
-function witnessActionRun(url, token) {
-    const action = new ActionRun(url);
-    const { name, status, conclusion, path, head_sha } = githubApiGet(action.url, token);
+function parseUrl(pageUrl) {
+    const match = pageUrl.match(/https:\/\/github.com\/([^\/]+)\/([^\/]+)\/actions\/runs\/(\d+)/);
+    if (!match) {
+        throw new Error("Invalid input url");
+    }
+    const [_all, owner, repo, runId] = match;
+    const repoUrl = `https://api.github.com/repos/${owner}/${repo}`;
+    const runUrl = `${repoUrl}/actions/runs/${runId}`;
+    const jobsUrl = `${runUrl}/jobs`;
+    const baseFileUrl = `${repoUrl}/contents`;
+    return { runUrl, jobsUrl, baseFileUrl };
+}
+
+function witness(pageUrl, token) {
+    const { runUrl, jobsUrl, baseFileUrl } = parseUrl(pageUrl);
+    const { name, status, conclusion, path, head_sha } = ghGet(runUrl, token);
     if (status != "completed") {
         throw new Error("Action run not completed");
     }
     if (conclusion !== 'success') {
         throw new Error("Action run failed");
     }
-    const workflowFile = githubApiGet(`${action.baseFileUrl}${path}?ref=${head_sha}`, token, true);
+    const workflowFile = ghGet(`${baseFileUrl}/${path}?ref=${head_sha}`, token, true);
     const workflowHash = hex(pink.hash("sha256", workflowFile));
-    const jobs = githubApiGet(action.jobsUrl, token);
+    const jobs = ghGet(jobsUrl, token);
     const notes = jobs.jobs.flatMap(job => {
         const { name, check_run_url } = job;
         const PREFIX = 'witness.';
-        return githubApiGet(`${check_run_url}/annotations`, token)
+        return ghGet(`${check_run_url}/annotations`, token)
             .filter(annotation => annotation.title.startsWith(PREFIX))
             .map(({ title, message }) => ({
                 title: title.slice(PREFIX.length),
@@ -92,13 +71,9 @@ function witnessActionRun(url, token) {
         name,
         path,
         gitCommit: head_sha,
-        runUrl: url,
+        runUrl: pageUrl,
         notes,
     });
 }
 
-(function () {
-    const actionUrl = scriptArgs[0];
-    const ghToken = scriptArgs[1];
-    return witnessActionRun(actionUrl, ghToken);
-}())
+witness(scriptArgs[0], scriptArgs[1]);
